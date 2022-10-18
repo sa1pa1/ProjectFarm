d31a39844227fafb1eb71c179e0410b4a18d7895
sa1pa1 <104730550+sa1pa1@users.noreply.github.com>
2022-10-18 19:51:18 +1030

    Add files via upload

--- WateringCan.cpp
+++ WateringCan.cpp
@@ -1,66 +1,54 @@
-/*
- * Implementation of the watering can class
- *
- * This file creates and initialises the equipment subclass: watering can
- * It initialises all of the data members in the watering can class and implements
- * its function members.
- *
- * To start the game, the user has a level 1 watering can in their inventory.
- * watering can can be upgraded to improve growth time.
- * The upgrade can be bought for 100 coins for lvl 2 and 200 for lvl 3.
- */
 #include "WateringCan.h"
-
-#include <iostream>
 #include <string>
+#include <iostream>
 using namespace std;
 
-// default watering can level. As player start with Wateringcan of level 1.
-WateringCan::WateringCan() {
-  equip_name = "Watering Can";
-  util_boost = 1; // The multiplier for growth time
-  util_lvl = 1; // upgrade level
-  equip_value = 100; // The value of the equipment increases as you upgrade
+//default of watering can 
+WateringCan::WateringCan(){
+    equip_name = "Watering Can";
+    util_boost = 1;
+    util_lvl = 1;
+    equip_value = 0;
+
 }
 
-// returns a short description with multiplier value depending on the upgrade
-// level of the watering can
-void WateringCan::get_Function() {
-  if (util_lvl == 1) {
-    cout << "Watering can of level " << get_util_lvl()
-         << ". No reduced growth time." << endl;
-  }
-  if (util_lvl == 2) {
-    cout << "Watering can of level " << get_util_lvl()
-         << " reduce growth time by 2.00x" << endl;
-  }
-  if (util_lvl == 3) {
-    cout << "Watering can of level " << get_util_lvl()
-         << " reduce growth time by 4.00x" << endl;
-  }
+void WateringCan::get_Function(){
+    if (util_lvl == 1){
+        cout<<"Watering can of level "<<get_util_lvl()<<" reduce growth time by 0.00x"<<endl;
+    }
+    if (util_lvl == 2){
+         cout<<"Watering can of level "<<get_util_lvl()<<" reduce growth time by 2x"<<endl;
+    }
+    if (util_lvl == 3){
+         cout<<"Watering can of level "<<get_util_lvl()<<" reduce growth time by 3x"<<endl;
+    }
+
 }
 
-// upgrade function to determine what level the watering can is
-void WateringCan::Upgrade() {
-  if (util_lvl < 3) {
+void WateringCan::Upgrade(){
+    if (util_lvl < 3){
     util_lvl = util_lvl + 1;
-    util_boost = 2;
-    equip_value = 100;
-  }
+    util_boost = util_boost +1;
+    equip_value = equip_value + 100;
+    }
 
-  if (util_lvl == 3) {
-    cout << "Maximum utility level reached" << endl;
-    util_lvl = 3;
-    util_boost = 4;
-    equip_value = 200;
-  }
+}
+    
+
+
+
+int WateringCan::get_util_lvl(){
+    return util_lvl;
 }
 
-// returns watering can upgrade level
-int WateringCan::get_util_lvl() { return util_lvl; }
-// returns watering can name
-string WateringCan::get_name() { return equip_name; }
-// returns watering can multiplier
-int WateringCan::get_boost_val() { return util_boost; }
-// returns watering can value
-int WateringCan::get_equip_val() { return equip_value; }
+string WateringCan::get_name(){
+    return equip_name;
+}
+
+int WateringCan::get_boost_val(){
+    return util_boost;
+}
+
+int WateringCan::get_equip_val(){
+    return equip_value;
+}
