ee99dc63575cb230ee3d5a2d964ad9de60627a84
AutumnSage <109932748+AutumnSage@users.noreply.github.com>
2022-10-18 15:50:15 +1030

    Update Fertiliser.cpp

--- Fertiliser.cpp
+++ Fertiliser.cpp
@@ -1,3 +1,14 @@
+/**
+ * Implementation of the Fertiliser class
+ *
+ * This file creates and initialises the equipment subclass: Fertiliser
+ * It initialises all of the data members in the Fertiliser class and implements
+ * its function members.
+ *
+ * To start the game, the user has a level 1 fertiliser in their inventory.
+ * Fertiliser can be upgraded to improve crop output. 
+ * The upgrade can be bought for 100 coins for lvl 2 and 200 for lvl 3.
+ */
 #include "Fertiliser.h"
 #include <string>
 #include <iostream>
@@ -8,12 +19,12 @@ using namespace std;
 
 Fertiliser::Fertiliser(){
     equip_name = "Fertiliser";
-    util_lvl = 1;
-    util_boost = 1;
-    equip_value = 0;
+    util_lvl = 1; // upgrade level
+    util_boost = 1; // The multiplier for crop output
+    equip_value = 0; // The value of the equipment increases as you upgrade
 }
 
-
+// returns a short description with multiplier value depending on the upgrade level of the fertiliser
 void Fertiliser::get_Function(){
     if (util_lvl == 1){
        cout<<"Fertiliser of level 1 help boost crop value by 0.00x"<<endl;
@@ -29,6 +40,7 @@ void Fertiliser::get_Function(){
     }
 }
 
+// upgrade function to determine what level the fertiliser is
 void Fertiliser::Upgrade(){
     if (util_lvl < 3){
     util_lvl = util_lvl + 1;
@@ -44,16 +56,19 @@ void Fertiliser::Upgrade(){
     }
 }
 
+// returns fertiliser upgrade level
 int Fertiliser::get_util_lvl(){
     return util_lvl;
 }
+// returns fertilisers name
 string Fertiliser::get_name(){
     return equip_name;
 }
+// returns fertilisers multiplier
 int Fertiliser::get_boost_val(){
     return util_boost;
 }
-
+// returns fertilisers value
 int Fertiliser::get_equip_val(){
     return equip_value;
 }
