2c752a5d0b636fc7bbd9933467526027d14409ab
AutumnSage <109932748+AutumnSage@users.noreply.github.com>
2022-10-18 17:28:26 +1030

    Update GardeningHoe.cpp

--- GardeningHoe.cpp
+++ GardeningHoe.cpp
@@ -1,3 +1,14 @@
+/**
+ * Implementation of the gardening hoe class
+ *
+ * This file creates and initialises the equipment subclass: gardening hoe
+ * It initialises all of the data members in the Fertiliser class and implements
+ * its function members.
+ *
+ * To start the game, the user has a level 1 gardening hoe in their inventory.
+ * gardening hoe can be upgraded to improve crop output.
+ * The upgrade can be bought for 100 coins for lvl 2 and 200 for lvl 3.
+ */
 #include "GardeningHoe.h"
 
 #include <iostream>
@@ -7,12 +18,14 @@ using namespace std;
 // default of Gardening Hoe
 GardeningHoe::GardeningHoe() {
   equip_name = "Gardening Hoe";
-  util_boost = 1;
-  util_lvl = 1;
-  equip_value = 100;
+  util_boost = 1; // The multiplier for crop output 
+  util_lvl = 1; //  upgrade level
+  equip_value = 100; // equipment value
 }
 
 // inspection constructor
+// returns a short description with multiplier value depending on the upgrade
+// level of the gardening hoe
 void GardeningHoe::get_Function() {
   if (util_lvl == 1) {
     cout << "Gardening Hoe of level 1. No boost in crop output";
@@ -27,6 +40,7 @@ void GardeningHoe::get_Function() {
   }
 }
 
+// upgrade function to determine what level the gardening hoe is
 void GardeningHoe::Upgrade() {
   if (util_lvl < 3) {
     util_lvl = util_lvl + 1;
@@ -42,10 +56,11 @@ void GardeningHoe::Upgrade() {
   }
 }
 
+// returns gardening hoe upgrade level
 int GardeningHoe::get_util_lvl() { return util_lvl; }
-
+// returns gardening hoe name
 string GardeningHoe::get_name() { return equip_name; }
-
+// returns gardening hoe multiplier
 int GardeningHoe::get_boost_val() { return util_boost; }
-
+// returns gardening hoe value
 int GardeningHoe::get_equip_val() { return equip_value; }
