3daf7e4308f80d17e5e2f3568cf8ea187e5d8362
sa1pa1 <104730550+sa1pa1@users.noreply.github.com>
2022-10-18 19:55:25 +1030

    Add files via upload

---
+++ tests/test_Fertiliser.cpp
@@ -0,0 +1,49 @@
+#include "WateringCan.cpp"
+#include "GardeningHoe.cpp"
+#include "Fertiliser.cpp"
+#include "Equipment.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    Fertiliser a;
+
+    cout<<a.get_name()<<endl;
+    cout<<"Utility level: "<<a.get_util_lvl()<<endl;
+    cout<<"Boost level: "<<a.get_boost_val()<<endl;
+    cout<<"Equipment value: "<<a.get_equip_val()<<endl;
+    a.get_Function();
+    cout<<endl;
+    cout<<endl;
+    //testing first upgradde
+    a.Upgrade();
+    cout<<"Utility level: "<<a.get_util_lvl()<<endl;
+    cout<<"Boost level: "<<a.get_boost_val()<<endl;
+    cout<<"Equipment value: "<<a.get_equip_val()<<endl;
+    a.get_Function();
+    cout<<endl;
+    cout<<endl;
+    //testing second upgrade
+    a.Upgrade();
+    cout<<"Utility level: "<<a.get_util_lvl()<<endl;
+    cout<<"Boost level: "<<a.get_boost_val()<<endl;
+     cout<<"Equipment value: "<<a.get_equip_val()<<endl;
+    a.get_Function();
+    cout<<endl;
+    cout<<endl;
+    //testing boundaries
+    a.Upgrade();
+    cout<<"Utility level: "<<a.get_util_lvl()<<endl;
+    cout<<"Boost level: "<<a.get_boost_val()<<endl;
+     cout<<"Equipment value: "<<a.get_equip_val()<<endl;
+    a.get_Function();
+    cout<<endl;
+
+}
+    
+
+    
+
+    
---
+++ tests/test_GardeningHoe.cpp
@@ -0,0 +1,39 @@
+#include "WateringCan.cpp"
+#include "GardeningHoe.cpp"
+#include "Fertiliser.cpp"
+#include "Equipment.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    GardeningHoe a;
+
+    cout<<a.get_name()<<endl;
+    cout<<"Utility level: "<<a.get_util_lvl()<<endl;
+    cout<<"Boost level: "<<a.get_boost_val()<<endl;
+    a.get_Function();
+    cout<<endl;
+    cout<<endl;
+    //testing first upgradde
+    a.Upgrade();
+    cout<<"Utility level: "<<a.get_util_lvl()<<endl;
+    cout<<"Boost level: "<<a.get_boost_val()<<endl;
+    a.get_Function();
+    cout<<endl;
+    cout<<endl;
+    //testing second upgrade
+    a.Upgrade();
+    cout<<"Utility level: "<<a.get_util_lvl()<<endl;
+    cout<<"Boost level: "<<a.get_boost_val()<<endl;
+    a.get_Function();
+    cout<<endl;
+    cout<<endl;
+    //testing boundaries
+    a.Upgrade();
+    cout<<"Utility level: "<<a.get_util_lvl()<<endl;
+    cout<<"Boost level: "<<a.get_boost_val()<<endl;
+    a.get_Function();
+
+}
